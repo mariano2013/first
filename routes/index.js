8fb0561458c92082dfd3c45c8af59e17f17cf98f
mariano2013 <mariano2013@users.noreply.github.com>
2014-10-28 16:16:58 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express in C9' });
 });
 
 module.exports = router;
